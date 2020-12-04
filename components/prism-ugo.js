28d2a0de6c0503f0e4e6dc37607cad2e5ed92b13
Ozan HACIBEKIROGLU <ozan@verigraf.com>
2020-12-04 13:42:46 +0300

    add const keyword

--- components/prism-ugo.js
+++ components/prism-ugo.js
@@ -1,5 +1,5 @@
 Prism.languages.ugo = Prism.languages.extend('clike', {
-	'keyword': /\b(?:break|continue|else|for|func|if|import|return|var|param|global|try|catch|finally|throw|in|undefined)\b/,
+	'keyword': /\b(?:break|continue|else|for|func|if|import|return|var|const|param|global|try|catch|finally|throw|in|undefined)\b/,
 	'builtin': /\b(?:bool|bytes|error|float|chars?|string|u?int|append|copy|delete|len|printf?(?:ln)?|sort|sortReverse|repeat|contains|typeName|sprintf|isError|isInt|isUint|isFloat|isChar|isBool|isString|isBytes|isMap|isSyncMap|isArray|isUndefined|isFunction|isCallable|isIterable|WrongNumArgumentsError|InvalidOperatorError|IndexOutOfBoundsError|NotIterableError|NotIndexableError|NotIndexAssignableError|NotCallableError|NotImplementedError|ZeroDivisionError|TypeError)\b/,
 	'boolean': /\b(?:_|true|false)\b/,
 	'operator': /[*\/%^!=]=?|\+[=+]?|-[=-]?|\|[=|]?|&(?:=|&|\^=?)?|>(?:>=?|=)?|<(?:<=?|=|-)?|:=|\.\.\./,
